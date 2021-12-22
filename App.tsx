d3ccf8190e553c42da7eb3b3497546a89d4847d3
Carlos Daniel <carloscar12carl@gmail.com>
2021-12-22 09:42:51 -0300

    atualizar

--- App.tsx
+++ App.tsx
@@ -1,26 +1,197 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import React, {Component} from 'react'; 
 
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 
+import React, {Component} from 'react';
 
-export default function App() {
+type Props ={};
+
+export default class App extends Component<Props>{
+
+  constructor(props){
+
+    super(props)
+
+    this.state = {altura:0, massa:0, resultado:0, resultadoText:""}
+
+    this.calcular = this.calcular.bind(this)
+
+  }
+
+ 
+
+  calcular(){
+
+ 
+
+    let imc = this.state.massa/ (this.state.altura * this.state.altura)
+
+    let s = this.state
+
+    s.resultado = imc
+
+    this.setState(s)
+
+   
+
+ 
+
+    if(s.resultado <16){
+
+      s.resultadoText = "Magreza Grave"
+
+ 
+
+    } else  if(s.resultado <17){
+
+      s.resultadoText = "Magreza Moderada"
+
+ 
+
+    } else  if(s.resultado <18.5){
+
+      s.resultadoText = "Magreza Leve"
+
+ 
+
+    } else  if(s.resultado <25){
+
+      s.resultadoText = "Saúdável"
+
+ 
+
+    } else  if(s.resultado <30){
+
+      s.resultadoText = "Sobrepeso"
+
+ 
+
+    } else  if(s.resultado <35){
+
+      s.resultadoText = "Obesidade Grau I"
+
+ 
+
+    } else  if(s.resultado <40){
+
+      s.resultadoText = "Obesidade Grau II"
+
+    } else {
+
+ 
+
+      s.resultadoText = "Obesidade Grau III"
+
+ 
+
+   
+
+    }
+
+ 
+
+    this.setState(s)
+
+  }
+
+ 
+
+  render(){
+
   return (
+
     <View style={styles.container}>
 
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <TextInput placeholder="Altura: " keyboardType='numeric'/>
-      <TextInput placeholder="Massa: " keyboardType='numeric'/>
-      <StatusBar style="auto" />
+     <View style={styles.entradas}>
+
+      <TextInput placeholder='Altura ' keyboardType='numeric' style={styles.input} onChangeText={(massa)=>{this.setState({massa})}}/>
+
+      <TextInput placeholder='Massa ' keyboardType='numeric'  style={styles.input} onChangeText={(altura)=>{this.setState({altura})}}/>
+
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={this.calcular}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
+
+      <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+
+      <Text style={[styles.resultado, {fontSize:35}]}>{this.state.resultadoText}</Text>
+
     </View>
+
   );
-}
+
+  }}
+
+ 
 
 const styles = StyleSheet.create({
+
   container: {
+
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    backgroundColor: '#F5FCFF',
+
   },
+
+  entradas:{
+
+    flexDirection: 'row',
+
+ 
+
+  },
+
+ 
+
+  input: {
+
+    height:80,
+
+    textAlign:"center",
+
+    width:"50%",
+
+    fontSize:50,
+
+    marginTop:24,
+
+  },
+
+  button:{
+
+    backgroundColor: '#89ffa5',
+
+  },
+
+ 
+
+  buttonText:{
+
+    alignSelf:'center',
+
+    padding: 30,
+
+    fontSize: 25,
+
+    color: "#6dc4a4",
+
+    fontWeight: 'bold',
+
+  },
+
+ 
+
+  resultado:{
+
+    alignSelf: "center",
+
+    color: "lightgray",
+
+    fontSize: 65,
+
+    padding:15,
+
+  },
+
 });
